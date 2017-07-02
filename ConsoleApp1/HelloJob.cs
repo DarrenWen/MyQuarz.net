b31a7780a4dde896959d71ce132b9e03e911691a
fu wenjun <fu wenjun>
2017-07-02 21:15:19 +0800

    添加项目文件。

---
+++ ConsoleApp1/HelloJob.cs
@@ -0,0 +1,29 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+
+            //Task t1 = new Task(MyMethod);
+
+            Random rd = new Random();
+
+            //t1.Start();
+            await Task.Delay(rd.Next(1,5)*1000);
+            MyMethod();
+            //return t1;
+        }
+
+        private void MyMethod()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            AsyncMethod();
+
+            Console.ReadLine();
+        }
+
+        static async void AsyncMethod()
+        {
+            try
+            {
+                // Grab the Scheduler instance from the Factory 
+                IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler();
+
+                // and start it off
+                scheduler.Start();
+
+                // define the job and tie it to our HelloJob class
+                IJobDetail job = JobBuilder.Create<HelloJob>()
+                    .WithIdentity("job1", "group1")
+                    .Build();
+
+                // Trigger the job to run now, and then repeat every 10 seconds
+                ITrigger trigger = TriggerBuilder.Create()
+                    .WithIdentity("trigger1", "group1")
+                    .StartNow()
+                    .WithSimpleSchedule(x => x
+                        .WithIntervalInSeconds(2)
+                        .RepeatForever())
+                    .Build();
+
+                // Tell quartz to schedule the job using our trigger
+                scheduler.ScheduleJob(job, trigger);
+
+                // some sleep to show what's happening
+                Thread.Sleep(TimeSpan.FromSeconds(60));
+
+                // and last shut down the scheduler when you are ready to close your program
+                scheduler.Shutdown();
+            }
+            catch (SchedulerException se)
+            {
+                Console.WriteLine(se);
+            }
+        }
+    }
+}
